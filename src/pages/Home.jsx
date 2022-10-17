e5de0f509768293de60ea065ffdb71004897a4e0
Jawad Abdulrazzaq <62211551+notjawad@users.noreply.github.com>
2022-10-17 14:27:47 -0500

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div>
+            <h1>Home</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/register">Register</a>
+                    </li>
+                    <li>
+                        <a href="/login">Login</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 };
 
 export default Home;
